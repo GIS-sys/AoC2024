ff23a2df78010e40a4a70c2dcb5e0585ad5d5de2
GISwarm <gordeiegorov@gmail.com>
2024-12-11 03:46:12 +0300

    6 day part 2 trying to add map for moving faster

--- 06b/main.py
+++ 06b/main.py
@@ -1,36 +1,46 @@
 DEBUG = True
 
+def hash(pair: tuple[int, int]) -> int:
+    return pair[0] * 3 + pair[1]
 
 def copy(x: list[list]) -> list[list]:
     return [[b for b in a] for a in x]
 
 
+class Field:
+    def __init__(self, field: list[list[int]]):
+        pass
+
+    d
+
 class Solver:
     def __init__(self):
-        self.field = []
+        self.field = None
         self.x, self.y = 0, 0
         self.width, self.height = 0, 0
 
     def read(self):
+        field = []
         while i := input():
             if "^" in i:
                 self.y = len(self.field)
                 self.x = i.index("^")
-            self.field.append(list(i))
+            field.append(list(i))
+        self.field = Field(field)
         self.width = len(self.field[0])
         self.height = len(self.field)
 
     def check_loop(self, fill_path_field: bool) -> bool:
         field = self.field
         if fill_path_field:
-            field = copy(self.field)
+            field = self.field.copy()
         x = self.x
         y = self.y
 
         dir = (0,-1)
         if fill_path_field:
-            field[y][x] = "X"
-        used = [[set() for __ in _] for _ in field]
+            field.set(x, y, "X")
+        used = [[set() for __ in self.width] for _ in self.height]
 
         while x >= 0 and x < self.width and y >= 0 and y < self.height and hash(dir) not in used[y][x]:
             used[y][x].add(hash(dir))
