9befcb769e0139078ab0b7f77cbb43d252ea3f6e
GISwarm <gordeiegorov@gmail.com>
2024-12-26 02:39:42 +0300

    task 21 part 1

--- 21a/main.py
+++ 21a/main.py
@@ -1,25 +1,73 @@
-DEBUG = True
+DEBUG = False
 
 
 class Keypad:
     @classmethod
-    def how_to_press(cls, key_from: str, key_to: str) -> str:
-        pos_from = cls.POSITIONS[key_from]
-        pos_to = cls.POSITIONS[key_to]
-        return cls.how_to_press_pos(pos_from, pos_to)
+    def find_specific_sequence(cls, code: str) -> tuple[str, int]:
+        code = "A" + code
+        sequence = ""
+        weight = 0
+        for i in range(0, len(code) - 1):
+            key_from, key_to = code[i], code[i + 1]
+            part_sequence, part_weight = cls.how_to_press(key_from, key_to)
+            sequence += part_sequence
+            weight += part_weight
+        return sequence, weight
 
     @classmethod
-    def how_to_press_pos(cls, pos_from: tuple[int, int], pos_to: tuple[int, int]) -> str:
-        # parse arguments
-        pos_from_x, pos_from_y = pos_from
-        pos_to_x, pos_to_y = pos_to
+    def how_to_press(cls, key_from: str, key_to: str) -> tuple[str, int]:
+        pos_from = cls.POSITIONS[key_from]
+        pos_to = cls.POSITIONS[key_to]
+        return cls.how_to_press_pos(pos_from[0], pos_from[1], pos_to[0], pos_to[1])
+
+    @classmethod
+    def how_to_press_pos(cls, pos_from_x: int, pos_from_y: int, pos_to_x: int, pos_to_y: int) -> tuple[str, int]:
+        return "?", 0
+
+
+class DirectionalKeypad(Keypad):
+    POSITIONS = {
+        "^": (1, 0),
+        "A": (2, 0),
+        "<": (0, 1),
+        "v": (1, 1),
+        ">": (2, 1),
+    }
+    FIELD_WIDTH = 3
+    FIELD_HEIGHT = 2
+    BAD_POS = (0, 0)
+
+    @classmethod
+    def how_to_press_pos(cls, pos_from_x: int, pos_from_y: int, pos_to_x: int, pos_to_y: int) -> str:
         # calc horizontal and vertical adjustments
         hor = ("<" if pos_from_x > pos_to_x else ">") * abs(pos_from_x - pos_to_x)
         ver = ("^" if pos_from_y > pos_to_y else "v") * abs(pos_from_y - pos_to_y)
         # combine them to prioritize hor and avoid bad pos
+        result = hor + ver + "A"
         if pos_from_y == cls.BAD_POS[1] and pos_to_x == cls.BAD_POS[0]:
-            return ver + hor + "A"
-        return hor + ver + "A"
+            result = ver + hor + "A"
+        return result, len(result)
+
+
+class DirectionalIntermediateKeypad(DirectionalKeypad):
+    @classmethod
+    def how_to_press_pos(cls, pos_from_x: int, pos_from_y: int, pos_to_x: int, pos_to_y: int) -> str:
+        # calc horizontal and vertical adjustments
+        hor = ("<" if pos_from_x > pos_to_x else ">") * abs(pos_from_x - pos_to_x)
+        ver = ("^" if pos_from_y > pos_to_y else "v") * abs(pos_from_y - pos_to_y)
+        # combine them to minimize length after next directional keypad
+        if pos_from_y == cls.BAD_POS[1] and pos_to_x == cls.BAD_POS[0]:
+            result = ver + hor + "A"
+            return result, DirectionalKeypad.find_specific_sequence(result)[1]
+        if pos_from_x == cls.BAD_POS[0] and pos_to_y == cls.BAD_POS[1]:
+            result = hor + ver + "A"
+            return result, DirectionalKeypad.find_specific_sequence(result)[1]
+        _, hor_first = DirectionalKeypad.find_specific_sequence(hor + ver + "A")
+        _, ver_first = DirectionalKeypad.find_specific_sequence(ver + hor + "A")
+        if hor_first < ver_first:
+            return hor + ver + "A", hor_first
+        else:
+            return ver + hor + "A", ver_first
 
 
 class NumericKeypad(Keypad):
@@ -40,18 +88,29 @@ class NumericKeypad(Keypad):
     FIELD_HEIGHT = 4
     BAD_POS = (0, 3)
 
-
-class DirectionalKeypad(Keypad):
-    POSITIONS = {
-        "^": (1, 0),
-        "A": (2, 0),
-        "<": (0, 1),
-        "v": (1, 1),
-        ">": (2, 1),
-    }
-    FIELD_WIDTH = 3
-    FIELD_HEIGHT = 2
-    BAD_POS = (0, 0)
+    @classmethod
+    def how_to_press_pos(cls, pos_from_x: int, pos_from_y: int, pos_to_x: int, pos_to_y: int) -> str:
+        # calc horizontal and vertical adjustments
+        hor = ("<" if pos_from_x > pos_to_x else ">") * abs(pos_from_x - pos_to_x)
+        ver = ("^" if pos_from_y > pos_to_y else "v") * abs(pos_from_y - pos_to_y)
+        # combine them to minimize length after next directional keypad
+        if pos_from_y == cls.BAD_POS[1] and pos_to_x == cls.BAD_POS[0]:
+            result = ver + hor + "A"
+            return result, DirectionalIntermediateKeypad.find_specific_sequence(result)[1]
+        if pos_from_x == cls.BAD_POS[0] and pos_to_y == cls.BAD_POS[1]:
+            result = hor + ver + "A"
+            return result, DirectionalIntermediateKeypad.find_specific_sequence(result)[1]
+        hfs, hor_first = DirectionalIntermediateKeypad.find_specific_sequence(hor + ver + "A")
+        vfs, ver_first = DirectionalIntermediateKeypad.find_specific_sequence(ver + hor + "A")
+        if DEBUG:
+            print(f"{hor_first=} {ver_first=}")
+            print(f"{hfs=} {vfs=}")
+            print(f"{hor=} {ver=}")
+            print(f"{pos_from_x=} {pos_from_y=}   {pos_to_x=} {pos_to_y=}")
+        if hor_first < ver_first:
+            return hor + ver + "A", hor_first
+        else:
+            return ver + hor + "A", ver_first
 
 
 class Solver:
@@ -63,25 +122,14 @@ class Solver:
         while i := input():
             self.codes.append(i)
 
-    def find_specific_sequence(self, code: str, keypad: Keypad):
-        code = "A" + code
-        sequence = ""
-        for i in range(0, len(code) - 1):
-            key_from, key_to = code[i], code[i + 1]
-            part_sequence = keypad.how_to_press(key_from, key_to)
-            sequence += part_sequence
-        return sequence
-
     def find_sequence(self, code: str):
-        robot1 = self.find_specific_sequence(code, NumericKeypad)
-        robot2 = self.find_specific_sequence(robot1, DirectionalKeypad)
-        #robot3 = self.find_specific_sequence(robot2, DirectionalKeypad)
-        human = self.find_specific_sequence(robot2, DirectionalKeypad)
+        robot1 = NumericKeypad.find_specific_sequence(code)[0]
+        robot2 = DirectionalIntermediateKeypad.find_specific_sequence(robot1)[0]
+        human = DirectionalKeypad.find_specific_sequence(robot2)[0]
         if DEBUG:
             print(code)
             print(robot1)
             print(robot2)
-            #print(robot3)
             print(human)
         return human
 
@@ -89,7 +137,8 @@ class Solver:
         result = 0
         for code in self.codes:
             sequence = self.find_sequence(code)
-            print(len(sequence))
+            if DEBUG:
+                print(len(sequence))
             complexity = int(code[:-1]) * len(sequence)
             result += complexity
         return result
