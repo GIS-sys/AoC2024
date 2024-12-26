3d2df7d64433f90d9a8759a668f63ff99a3c4fb4
GISwarm <gordeiegorov@gmail.com>
2024-12-26 08:47:23 +0300

    task 24 part 1

---
+++ 24a/main.py
@@ -0,0 +1,89 @@
+DEBUG = True
+
+
+class ComputeVertice:
+    def __init__(self, name: str):
+        self.name = name
+        self.value = None
+
+
+class ComputeEdge:
+    def __init__(self, in0: ComputeVertice, in1: ComputeVertice, out: ComputeVertice, operation: str):
+        self.in0 = in0
+        self.in1 = in1
+        self.out = out
+        self.operation = operation
+
+    def calculate(self) -> int:
+        if self.operation == "OR":
+            return self.in0.value | self.in1.value
+        if self.operation == "AND":
+            return self.in0.value & self.in1.value
+        if self.operation == "XOR":
+            return self.in0.value ^ self.in1.value
+
+
+class ComputeGraph:
+    def __init__(self, edges: list[tuple[str, str, str, str]]):
+        self.vertices: dict[str, ComputeVertice] = {}
+        self.output: dict[str, list[ComputeEdge]] = {}
+        self.input: dict[str, ComputeEdge] = {}
+        # add all vertices
+        for input1, input2, output, operation in edges:
+            self.vertices[input1] = ComputeVertice(input1)
+            self.vertices[input2] = ComputeVertice(input2)
+            self.vertices[output] = ComputeVertice(output)
+        # add edges
+        for input1, input2, output, operation in edges:
+            edge = ComputeEdge(self.vertices[input1], self.vertices[input2], self.vertices[output], operation)
+            self.output[input1] = self.output.get(input1, []) + [edge]
+            self.output[input2] = self.output.get(input2, []) + [edge]
+            self.input[output] = edge
+
+    def push(self, name: str, value: int):
+        self.vertices[name].value = value
+        for edge in self.output.get(name, []):
+            if edge.in0.value is not None and edge.in1.value is not None:
+                self.push(edge.out.name, edge.calculate())
+
+    def run(self, initial_values: list[tuple[str, int]]) -> list[tuple[str, int]]:
+        for name, value in initial_values:
+            self.push(name, value)
+        return [(v.name, v.value) for v in self.vertices.values()]
+
+
+class Solver:
+    def __init__(self):
+        self.initial_values: list[tuple[str, int]] = None
+        self.edges: list[tuple[str, str, str, str]] = None
+
+    def read(self):
+        self.initial_values = []
+        self.edges = []
+        while i := input():
+            name, value = i.split(": ")
+            value = int(value)
+            self.initial_values.append((name, value))
+        while i := input():
+            input1, operation, input2, _, output = i.split(" ")
+            self.edges.append((input1, input2, output, operation))
+
+    def solve(self):
+        graph = ComputeGraph(self.edges)
+        all_values = graph.run(self.initial_values)
+        if DEBUG:
+            print(f"{all_values=}")
+        all_values.sort()
+        z_values = [v[1] for v in all_values if v[0].startswith("z")]
+        result = 0
+        for i, v in enumerate(z_values):
+            result += v * 2**i
+        return result
+
+
+if __name__ == "__main__":
+    solver = Solver()
+    solver.read()
+    answer = solver.solve()
+    print(answer)
+
