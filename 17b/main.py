835966ea04025ea7d6785b3679ac17a1b47fcc45
GISwarm <gordeiegorov@gmail.com>
2024-12-27 03:38:13 +0300

    task 17 part 2 changing to hand-parsing

--- 17b/main.py
+++ 17b/main.py
@@ -1,39 +1,29 @@
-from multiprocessing import Pool
+# By analazing the program it looks like it's a single big cycle, which transforms
+# a    a//2^3
+# b -> (a%2^3)^(a//2^5)^6
+# c    a//2^5
+# and then outputs register b. In other words, if a = ak....a7a6a5a4a3a2a1, then program transforms
+# a      ak...a5a3
+# ... -> a2a1a0 ^ a7a6a5 ^ 110
+# ...    ak...a6a5
+# and outputs a2a1a0 ^ a7a6a5 ^ 110
+#
+# so we only need to create dependencies and then iterate over free elements
 
 
 DEBUG = False
 DEBUG_MAX_ITER = 100000
+MAX_NUMBER = 1_000_000_000_000_000
 
 
-MOCK_INPUT = False
-lines_real = [
-    "Register A: 52884621",
-    "Register B: 0",
-    "Register C: 0",
-    "",
-    "Program: 2,4,1,3,7,5,4,7,0,3,1,5,5,5,3,0"
-]
-lines_example = [
-    "Register A: 729",
-    "Register B: 0",
-    "Register C: 0",
-    "",
-    "Program: 0,1,5,4,3,0"
-]
-lines = lines_real
-#lines = lines_example
-MI = 0
-def mock_input():
- global MI, lines
- if MI >= len(lines):
-  raise Exception("mock input ended")
- MI += 1
- return lines[MI - 1]
-if MOCK_INPUT:
-    input = mock_input
-
-
-PROCESSES = 32
+def dec_to_bin(x: int, length: int = None) -> list[int]:
+    result = []
+    while x:
+        r, x = x % 2, x // 2
+        result.append(r)
+    if length:
+        result = result + [0] * (length - len(result))
+    return result[::-1]
 
 
 def xor(a, b):
@@ -42,80 +32,51 @@ def xor(a, b):
 
 class Solver:
     def __init__(self):
-        self.reg = None
-        self.program = None
+        self.bits_read_a = 3 # this means we take a2a1a0
+        self.bits_bias_a = 5 # this means we take same amount as before, but from 5: a7a6a5
+        self.bits_const = dec_to_bin(6, self.bits_read_a) # this represents 110
 
     def read(self):
-        self.reg = [input() for _ in range(3)]
-        self.reg = [int(x[x.index(": ") + 2:]) for x in self.reg]
+        [input() for _ in range(3)]
         input()
         self.program = input()
         self.program = self.program[self.program.index(": ") + 2:].split(",")
-        self.program = list(map(int, self.program)) + [None]
-
-    def model(self, prediction: list[int]):
-        output = []
-        ip = 0
-        iter = 0
-        program = self.program.copy()
-        reg = self.reg.copy()
-        while ip < len(program) - 1 and iter < DEBUG_MAX_ITER:
-            op = program[ip]
-            literal = program[ip + 1]
-            # parse combo
-            combo = ([0,1,2,3] + reg + [None])[literal]
-            # parse op
-            if op == 0:
-                reg[0] = reg[0] // 2**combo
-            elif op == 1:
-                reg[1] = xor(reg[1], literal)
-            elif op == 2:
-                reg[1] = combo % 8
-            elif op == 3:
-                if reg[0] != 0:
-                    ip = literal - 2
-            elif op == 4:
-                reg[1] = xor(reg[1], reg[2])
-            elif op == 5:
-                output.append(combo % 8)
-                if len(output) > len(prediction) or output[-1] != prediction[len(output) - 1]:
-                    return False
-            elif op == 6:
-                reg[1] = reg[0] // 2**combo
-            elif op == 7:
-                reg[2] = reg[0] // 2**combo
-            # if DEBUG:
-            #     print(f"{ip}: {op} <- {literal} ({combo})  {reg} {output}")
-            # next
-            ip += 2
-            iter += 1
-        return len(output) == len(prediction) - 1
-
-    @staticmethod
-    def iterate_solution(args):
-        self, start, step, length = args
-        a = start
-        for _ in range(length):
-            self.reg[0] = a
-            if self.model(self.program):
-                return a
-            a += step
-        return None
+        self.program = list(map(int, self.program))
 
     def solve(self):
-        ONE_STEP_LENGTH = 1_000_000
-        # iter = 10_000_000_000 // ONE_STEP_LENGTH // PROCESSES
-        iter = 16608_000_000 // ONE_STEP_LENGTH // PROCESSES
-        # iter = 0
-        while True:
-            with Pool(PROCESSES) as p:
-                results = p.map(Solver.iterate_solution, [(self.copy(), iter * ONE_STEP_LENGTH * PROCESSES + index, PROCESSES, ONE_STEP_LENGTH) for index in range(PROCESSES)])
-            for result in results:
-                if result is not None:
-                    return result
-            iter += 1
-            if DEBUG:
-                print(f"{iter * PROCESSES} * {ONE_STEP_LENGTH}")
+        # find dependencies based on output
+        dependencies: dict[int, tuple[int, bool]] = dict() # dependencies[x] = (y, a) <=> bit X is equal to bit Y (reversed if a==True)
+        for iter, out in enumerate(self.program):
+            out_bin = dec_to_bin(out, self.bits_read_a)
+            for i, (out_bit, const_bit) in enumerate(zip(out_bin, self.bits_const)):
+                dependencies[iter * self.bits_read_a + i + self.bits_bias_a] = (iter * self.bits_read_a + i, out_bit == const_bit)
+        # find free bits
+        free_bits_indexes = []
+        for index in range(max(dependencies.keys())):
+            if index not in dependencies:
+                free_bits_indexes.append(index)
+        # generate numbers using free bits and dependencies
+        min_number = MAX_NUMBER
+        for step in range(2**len(free_bits_indexes)):
+            free_bits = dec_to_bin(step, len(free_bits_indexes))
+            number: dict[int, int] = dict()
+            print(free_bits_indexes, free_bits)
+            print(number)
+            print(dependencies)
+            for bit, index in zip(free_bits, free_bits_indexes):
+                number[index] = bit
+            for next_index in sorted(dependencies.keys()):
+                prev_index, reverse = dependencies[next_index]
+                next_number = number[prev_index]
+                if reverse:
+                    next_number = 1 - next_number
+                number[next_index] = next_number
+            number_dec = 0
+            for index, bit in number.items():
+                number_dec += bit * 2**index
+            print(number_dec)
+            min_number = min(min_number, number_dec)
+        return min_number
 
     def copy(self):
         result = Solver()
