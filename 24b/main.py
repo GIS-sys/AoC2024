cc06f109b7b1618b1765144cc1bbc047b203f469
GISwarm <gordeiegorov@gmail.com>
2024-12-30 04:54:10 +0300

    task 24 part 2 add basic wire type data processing

--- 24b/main.py
+++ 24b/main.py
@@ -1,8 +1,28 @@
+"""
+f  z.. != PREV_XOR_XOR: self.values=[('4', 'z44')], self.wire='z45'
+f  z.. != PREV_XOR_XOR: self.values=[('2', 'z11')], self.wire='qhj'
+f  len(self.values) != 1: self.values=[], self.wire='crn'
+f  z.. != PREV_XOR_XOR: self.values=[('4', 'z06')], self.wire='z06'
+f  len(self.values) != 1: self.values=[], self.wire='cfp'
+f  z.. != PREV_XOR_XOR: self.values=[('2', 'z35')], self.wire='hqk'
+f  z.. != PREV_XOR_XOR: self.values=[('3', 'z11')], self.wire='z11'
+f  z.. != PREV_XOR_XOR: self.values=[('0', 'z00')], self.wire='z00'
+f  z.. != PREV_XOR_XOR: self.values=[('2', 'z06')], self.wire='fhc'
+f  len(self.values) != 1: self.values=[], self.wire='pdp'
+f  len(self.values) != 1: self.values=[], self.wire='mbg'
+f  len(self.values) != 1: self.values=[], self.wire='z23'
+f  z.. != PREV_XOR_XOR: self.values=[('1', 'z35')], self.wire='z35'
+13 ['z45', 'qhj', 'crn', 'z06', 'cfp', 'hqk', 'z11', 'z00', 'fhc', 'pdp', 'mbg', 'z23', 'z35']
+"""
+
+
 import random
-from typing import Union
+from typing import Any, Union
 
 
 DEBUG = True
+DEBUG_PRINT_CHECK_FALSE = True
+DEBUG_PRINT_CHECK_TRUE = False
 BAD_ATLEAST = 10000
 GOOD_ATLEAST = 1
 
@@ -115,6 +135,56 @@ class ComputeGraph:
         raise EdgeLocationException()
 
 
+class WireTypeData:
+    BASE_XOR = "0"
+    BASE_AND = "1"
+    PREV_XOR_XOR = "2"
+    PREV_XOR_AND = "3"
+    NEXT_OR = "4"
+
+    def __init__(self, wire: str):
+        self.values = []
+        self.wire = wire
+
+    def set(self, kind: str, z_vert_name: str):
+        self.values.append((kind, z_vert_name))
+
+    def check(self) -> bool:
+        # TODO may be a problem if locate_edge can return several, then we may incorrectly assign all system
+        # TODO check z_vert_name
+        if self.wire.startswith(("x", "y")):
+            return True
+        if len(self.values) != 1:
+            if DEBUG and DEBUG_PRINT_CHECK_FALSE:
+                print(f"f  len(self.values) != 1: {self}")
+            return False
+        kind, z_vert_name = self.values[0]
+        # prev xor xor must have out=z, and vice versa
+        if self.wire.startswith("z") != (kind == WireTypeData.PREV_XOR_XOR):
+            if DEBUG and DEBUG_PRINT_CHECK_FALSE:
+                print(f"f  z.. != PREV_XOR_XOR: {self}")
+            return False
+        # base xor and and are always correct
+        if kind == WireTypeData.BASE_XOR or kind == WireTypeData.BASE_AND:
+            if DEBUG and DEBUG_PRINT_CHECK_TRUE:
+                print(f"t  BASE_XOR or BASE_AND: {self}")
+            return True
+        # prev xor xor, prev xor and must have other input as previous NEXT_OR
+        if kind == WireTypeData.PREV_XOR_XOR or kind == WireTypeData.PREV_XOR_AND:
+            if DEBUG and DEBUG_PRINT_CHECK_FALSE:
+                print(f"f  PREV_XOR_XOR or PREV_XOR_AND: {self}")
+            return False  # TODO
+        # next or must be an input to next PREV_XOR_XOR and PREV_XOR_AND
+        if kind == WireTypeData.NEXT_OR:
+            if DEBUG and DEBUG_PRINT_CHECK_FALSE:
+                print(f"f  NEXT_OR: {self}")
+            return False  # TODO
+        raise Exception(f"wtf unknown kind: wire={self.wire}, {kind=}, {z_vert_name=}")
+
+    def __repr__(self) -> str:
+        return f"{self.values=}, {self.wire=}"
+
+
 class Solver:
     def __init__(self):
         self.initial_values: list[tuple[str, int]] = None
@@ -147,20 +217,38 @@ class Solver:
 
     def solve(self):
         graph = ComputeGraph(self.edges)
-        for z_vert_name in sorted([v for v in graph.vertices if v.startswith("z")])[1:]:
+        wire_to_type_data: dict[str, Any] = dict()
+        for v in graph.vertices:
+            wire_to_type_data[v] = WireTypeData(v)
+        for z_vert_name in sorted([v for v in graph.vertices if v.startswith("z")]):
             x_vert_name, y_vert_name = "x"+z_vert_name[1:], "y"+z_vert_name[1:]
             # find edges to wires: base_xor, base_and, prev_xor_xor, prev_xor_and, next_or
             try:
                 base_xor_edge = graph.locate_edge((x_vert_name, y_vert_name), "XOR")
+                wire_to_type_data[base_xor_edge.out.name].set(kind=WireTypeData.BASE_XOR, z_vert_name=z_vert_name)
+
                 base_and_edge = graph.locate_edge((x_vert_name, y_vert_name), "AND")
+                wire_to_type_data[base_and_edge.out.name].set(kind=WireTypeData.BASE_AND, z_vert_name=z_vert_name)
+
                 prev_xor_xor_edge = graph.locate_edge(base_xor_edge.out.name, "XOR")
+                wire_to_type_data[prev_xor_xor_edge.out.name].set(kind=WireTypeData.PREV_XOR_XOR, z_vert_name=z_vert_name)  # note that output is z_vert_name, other input is previous NEXT_OR
+
                 prev_xor_and_edge = graph.locate_edge(prev_xor_xor_edge.ins(), "AND")
+                wire_to_type_data[prev_xor_and_edge.out.name].set(kind=WireTypeData.PREV_XOR_AND, z_vert_name=z_vert_name)  # note that other input is previous NEXT_OR
+
                 next_or_edge = graph.locate_edge((base_and_edge.out.name, prev_xor_and_edge.out.name), "OR")
-                # TODO add more checks to ensure edges are correct, and add variable to store all edge and check if they repeat
+                wire_to_type_data[next_or_edge.out.name].set(kind=WireTypeData.NEXT_OR, z_vert_name=z_vert_name)
             except EdgeLocationException:
-                print(z_vert_name)
+                if DEBUG:
+                    print("Error while locating edge according to structure in", z_vert_name)
                 continue
-        result = 0
+        bad_wires = []
+        for wire, wtd in wire_to_type_data.items():
+            if not wtd.check():
+                bad_wires.append(wire)
+        if DEBUG:
+            print(len(bad_wires), bad_wires)
+        result = ",".join(bad_wires)
         return result
 
 
